4355ad81bb2bb792ede2ce39a8a48f7bfe4997c2
minion-K <rnjsalsgus06@naver.com>
2025-11-18 11:38:26 +0900

    1118 created User/UserRole/Role Entity

---
+++ board-back/src/main/java/org/example/boardback/common/apis/BoardCommentApi.java
@@ -0,0 +1,9 @@
+package org.example.boardback.common.apis;
+
+public class BoardCommentApi {
+    private BoardCommentApi () {}
+
+    // Comment
+    public static final String ROOT = ApiBase.BASE + "/boards/{boardId}/comments";
+    public static final String COMMITS_BY_ID = ROOT + "/{commentId}";
+}
